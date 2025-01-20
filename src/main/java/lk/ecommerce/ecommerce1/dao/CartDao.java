b844a2df36aab88fc4313d0e2923f032b6002bca
Maleesha Madhushanka <154691806+MaleeshaMAdhushanka@users.noreply.github.com>
2025-01-20 15:28:03 +0530

    Creating CartDao

--- src/main/java/lk/ecommerce/ecommerce1/dao/CartDao.java
+++ src/main/java/lk/ecommerce/ecommerce1/dao/CartDao.java
@@ -1,4 +1,206 @@
 package lk.ecommerce.ecommerce1.dao;
 
+import com.sun.jdi.request.StepRequest;
+import lk.ecommerce.ecommerce1.entities.Cart;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class CartDao {
+
+    private Connection con;
+
+    public CartDao(Connection con) {
+        this.con = con;
+    }
+
+    public boolean addToCart(Cart cart){
+        boolean flag = false;
+        try {
+        String query = "insert into cart(uid, pid, quantity) values (?,?,?)";
+        PreparedStatement pstm  = this.con.prepareStatement(query);
+        pstm.setInt(1, cart.getCartId());
+        pstm.setInt(2, cart.getProductId());
+        pstm.setInt(3, cart.getQuantity());
+
+        pstm.executeUpdate();
+        flag = true;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return flag;
+
+
+    }
+
+    public List<Cart> getCartListByUserId(int uid){
+        List<Cart> list = new ArrayList<Cart>();
+
+        try {
+         String query = "select  * from cart where uid = ?";
+           PreparedStatement pstm = this.con.prepareStatement(query);
+           pstm.setInt(1, uid);
+
+          ResultSet rs =  pstm.executeQuery();
+          while (rs.next()){
+              Cart cart = new Cart();
+              cart.setCartId(rs.getInt("id"));
+              cart.setUserId(rs.getInt("uid"));
+              cart.setProductId(rs.getInt("pid"));
+              cart.setQuantity(rs.getInt("quantity"));
+
+              list.add(cart);
+          }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
+
+    public int getQuantity(int uid, int pid){
+        int qty = 0;
+
+        try {
+          String query = "select * from cart where uid = ? and pid = ?";
+            PreparedStatement pstm = this.con.prepareStatement(query);
+            pstm.setInt(1, uid);
+            pstm.setInt(2, pid);
+           ResultSet rs =  pstm.executeQuery();
+           while (rs.next()){
+             qty  =  rs.getInt("quantity");
+           }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return qty;
+
+    }
+
+    public int getQuantityById(int id){
+        int qty = 0;
+        try {
+        String query = "select * from cart where id = ?";
+
+        PreparedStatement pstm = this.con.prepareStatement(query);
+        pstm.setInt(1, id);
+       ResultSet rs =  pstm.executeQuery();
+       while (rs.next()){
+          qty = rs.getInt("quantity");
+       }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return qty;
+    }
+
+    public void updateQuantity(int id, int qty) {
+
+        try {
+            String query = "update cart set quantity = ? where id = ?";
+            PreparedStatement psmt = this.con.prepareStatement(query);
+            psmt.setInt(1, qty);
+            psmt.setInt(2, id);
+
+            psmt.executeUpdate();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public void removeProduct(int cid){
+        try{
+            String query = "delete from cart where id = ?";
+            PreparedStatement pstm = this.con.prepareStatement(query);
+            pstm.setInt(1, cid);
+            pstm.executeUpdate();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    public void removeAllProduct(){
+        try {
+            String query = "delete from cart";
+            PreparedStatement pstm = this.con.prepareStatement(query);
+            pstm.executeUpdate();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public int getIdByUserIdAndProductId(int uid, int pid){
+        int cid = 0;
+        try {
+            String query = "select * from cart where uid = ? and pid = ?";
+            PreparedStatement pstm = this.con.prepareStatement(query);
+            pstm.setInt(1, uid);
+            pstm.setInt(2, pid);
+
+           ResultSet rs =  pstm.executeQuery();
+           while (rs.next()){
+             cid = rs.getInt("id");
+           }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return cid;
+    }
+
+
+    public int getCartCountByUserId(int uid){
+        int count = 0;
+
+        try {
+            String query = "select count(*) from cart where uid=?";
+            PreparedStatement pstm =  this.con.prepareStatement(query);
+            pstm.setInt(1, uid);
+
+             ResultSet rs = pstm.executeQuery();
+              rs.next();
+              count =  rs.getInt(1);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return count;
+
+    }
+
+    public int getProductId(int cid){
+        int pid = 0;
+
+        try {
+            String query = "select pid from cart where id=?";
+            PreparedStatement pstm = this.con.prepareStatement(query);
+            pstm.setInt(1, cid);
+            ResultSet rs =  pstm.executeQuery();
+            rs.next();
+            pid = rs.getInt(1);
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+
+        }
+        return pid;
+
+    }
+
+
+
+
+
+
 }
+

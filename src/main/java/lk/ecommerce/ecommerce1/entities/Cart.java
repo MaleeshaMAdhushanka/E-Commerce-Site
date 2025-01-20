0f53a1b87cc191309bcf6f18ccf61a5fff295f00
Maleesha Madhushanka <154691806+MaleeshaMAdhushanka@users.noreply.github.com>
2025-01-20 10:46:20 +0530

    Create Cart entity

--- src/main/java/lk/ecommerce/ecommerce1/entities/Cart.java
+++ src/main/java/lk/ecommerce/ecommerce1/entities/Cart.java
@@ -1,4 +1,46 @@
 package lk.ecommerce.ecommerce1.entities;
 
 public class Cart {
+
+
+    private int cartId;
+    private int userId;
+    private int productId;
+    private int quantity;
+
+    public Cart() {
+        super();
+    }
+
+    public int getCartId() {
+        return cartId;
+    }
+
+    public void setCartId(int cartId) {
+        this.cartId = cartId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
 }

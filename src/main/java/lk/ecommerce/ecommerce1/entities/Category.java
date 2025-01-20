867774e19e29ad70065d268011fd8ad400ed6fc0
Maleesha Madhushanka <154691806+MaleeshaMAdhushanka@users.noreply.github.com>
2025-01-20 11:04:16 +0530

    Create Category entity

--- src/main/java/lk/ecommerce/ecommerce1/entities/Category.java
+++ src/main/java/lk/ecommerce/ecommerce1/entities/Category.java
@@ -1,4 +1,50 @@
 package lk.ecommerce.ecommerce1.entities;
 
 public class Category {
+
+    private int categoryId;
+    private String categoryName;
+    private  String categoryImage;
+
+    private Category(){
+        super();
+    }
+
+    public Category(String categoryName, String categoryImage) {
+        super();
+        this.categoryName = categoryName;
+        this.categoryImage = categoryImage;
+    }
+
+    public Category(int categoryId, String categoryName, String categoryImage) {
+        super();
+        this.categoryId = categoryId;
+        this.categoryName = categoryName;
+        this.categoryImage = categoryImage;
+    }
+
+    public int getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(int categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    public String getCategoryImage() {
+        return categoryImage;
+    }
+
+    public void setCategoryImage(String categoryImage) {
+        this.categoryImage = categoryImage;
+    }
+
 }

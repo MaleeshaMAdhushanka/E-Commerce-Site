7dc18b1245d3da567051f5d6863bec19850c441a
Maleesha Madhushanka <154691806+MaleeshaMAdhushanka@users.noreply.github.com>
2025-01-20 11:10:50 +0530

    Create Message entity

--- src/main/java/lk/ecommerce/ecommerce1/entities/Message.java
+++ src/main/java/lk/ecommerce/ecommerce1/entities/Message.java
@@ -1,4 +1,45 @@
 package lk.ecommerce.ecommerce1.entities;
 
-public class Message {
+public class Message{
+
+    private String message;
+
+    private String messageType;
+
+    private String cssClass;
+
+
+    public Message() {
+    }
+
+    public Message(String message, String messageType, String cssClass) {
+        this.message = message;
+        this.messageType = messageType;
+        this.cssClass = cssClass;
+    }
+
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessageType() {
+        return messageType;
+    }
+
+    public void setMessageType(String messageType) {
+        this.messageType = messageType;
+    }
+
+    public String getCssClass() {
+        return cssClass;
+    }
+
+    public void setCssClass(String cssClass) {
+        this.cssClass = cssClass;
+    }
 }

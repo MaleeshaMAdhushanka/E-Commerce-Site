e3f7cbc2d14269e7a0f40cd7e7c88e2b8227cac0
Maleesha Madhushanka <154691806+MaleeshaMAdhushanka@users.noreply.github.com>
2025-01-20 11:17:01 +0530

    OrderedProduct entity

--- src/main/java/lk/ecommerce/ecommerce1/entities/OrderedProduct.java
+++ src/main/java/lk/ecommerce/ecommerce1/entities/OrderedProduct.java
@@ -1,4 +1,75 @@
 package lk.ecommerce.ecommerce1.entities;
 
 public class OrderedProduct {
+
+    private int id;
+    private String name;
+
+    private int quantity;
+
+    private float price;
+    private String image;
+
+    private int orderId;
+
+
+    public OrderedProduct() {
+        super();
+    }
+
+    public OrderedProduct(String name, int quantity, String image, float price, int orderId) {
+        this.name = name;
+        this.quantity = quantity;
+        this.image = image;
+        this.price = price;
+        this.orderId = orderId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
 }
